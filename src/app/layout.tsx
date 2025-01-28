7cd7de988d9019e612cb73cff674b3765905aa9a
naysonpedro25 <naysonpedro2020@gmail.com>
2025-01-27 22:26:15 -0300

    Adicionando o arquivo

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import CuPage from './subendpoint/page';
 export default function RootLayout ({children} : {children: React.ReactNode}){
+
     return (
         <html lang="pt-br">
             <body>
